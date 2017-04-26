ba236d500e987bb0eed8b163a1c54938eb59bcb6
Jaydon Carling <jaydon134@me.com>
2017-04-26 04:55:41 +0000

    Added skills with a many to many relationship.

--- peopleFinder/models.py
+++ peopleFinder/models.py
@@ -5,8 +5,18 @@ from django import forms
 
 # Create your models here.
 
-class People(models.Model):
-    first_name = models.CharField(max_length = 35)
-    last_name = models.CharField(max_length = 35)
-    phone_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$') 
-    email_address = forms.EmailField()
+class Person(models.Model):
+    first = models.CharField(max_length = 35)
+    last = models.CharField(max_length = 35)
+    phone = models.CharField(max_length = 10, default = '0000000000') 
+    email = models.EmailField(max_length = 254, default = 'No Email Available')
+
+    def __str__(self):
+        return "%s %s" %(self.first, self.last)
+
+class Skill(models.Model):
+    skill_name = models.CharField(max_length = 35)
+    person = models.ManyToManyField(Person, related_name = "skill_set")
+
+    def __str__(self):
+        return self.skill_name
